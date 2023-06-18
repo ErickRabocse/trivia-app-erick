3148a3ba2d941808ed8e385730a669714ed50b51
ErickRabocse <69663754+ErickRabocse@users.noreply.github.com>
2023-06-17 20:11:11 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Trivia App: sitio web que genere trivias API: 
+--> https://opentdb.com/api_config.php
+    https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple
+
+OK --> 10 preguntas, modificar la dificultad, eleccionar el tipo de respuesta, escoger la categoría.
+
+Una vez seleccionado las parámetros se crea la trivia
+--> mostrar: las preguntas y las posibles respuestas
+--> Se deben de contestar
+
+--> Cada pregunta correcta vale 100 puntos (Mostrar puntaje final)
+--> Botón de crear nueva trivia
+
+Cosas a tener en cuenta:Github pages 
+Extras: SCSS, BEM, Webpack */
