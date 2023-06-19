759729a37347403315ee1af4058b01905dd4a733
ErickRabocse <69663754+ErickRabocse@users.noreply.github.com>
2023-06-19 16:17:01 -0600

    answers are now shuffled

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-/* Trivia App: sitio web que genere trivias API: 
---> https://opentdb.com/api_config.php
-    https://opentdb.com/api.php?amount=10
+/* Trivia App: sitio web que genere trivias API: https://opentdb.com/api_config.php
     https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple
-OK --> 10 preguntas, modificar la dificultad, eleccionar el tipo de respuesta, escoger la categoría.
-Una vez seleccionado las parámetros se crea la trivia
---> mostrar: las preguntas y las posibles respuestas
---> Se deben de contestar
 --> Cada pregunta correcta vale 100 puntos (Mostrar puntaje final)
---> Botón de crear nueva trivia
 Cosas a tener en cuenta:Github pages 
 Extras: SCSS, BEM, Webpack */
 
+const api =
+  "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple";
 const triviaSection = document.querySelector(".trivia__creation");
 
-const triviaAPI = fetch(
-  "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
-);
+const triviaAPI = fetch(api);
 
 triviaAPI
   .then((response) => response.json())
   .then((data) => {
     let resultados = data.results;
+    /* DISPPLAYING RESULTS ON SCREEN */
     showData(resultados);
   })
   .catch((error) => console.log(error, error.message));
 
+//Function that shows data on the site
 const showData = (resultados) => {
-  console.log(resultados);
   let body = "";
   resultados.forEach((element, index) => {
+    //SHOWING RESULTS ON SCREEN
     body += `
         <div class="trivia">
           <div class="trivia__question">
@@ -60,7 +55,7 @@ const showData = (resultados) => {
               <li class="trivia__list--el">
                 <input type="radio" name="${
                   index + 1
-                } " class="trivia__answer" id="d">${
+                } " class="trivia__answer" id="d" class="correctOne">${
       element.correct_answer
     }</input>
               </li>
@@ -70,8 +65,15 @@ const showData = (resultados) => {
     `;
   });
   triviaSection.innerHTML = body;
+
+  //BUTTON
   triviaSection.insertAdjacentHTML(
     "beforeend",
     `<button id="submit">submit</button>`
   );
+  //SHUFFLE ANSWERS
+  let myList = document.querySelector(".trivia__list");
+  for (let i = myList.children.length; i >= 0; i--) {
+    myList.appendChild(myList.children[(Math.random() * i) | 0]);
+  }
 };
