54d47aff816ba63189f2effaf6d3444f9ef7c195
ErickRabocse <69663754+ErickRabocse@users.noreply.github.com>
2023-06-18 18:24:28 -0600

    API fetched and rendered for a single survey result

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
 /* Trivia App: sitio web que genere trivias API: 
 --> https://opentdb.com/api_config.php
+    https://opentdb.com/api.php?amount=10
     https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple
-
 OK --> 10 preguntas, modificar la dificultad, eleccionar el tipo de respuesta, escoger la categoría.
-
 Una vez seleccionado las parámetros se crea la trivia
 --> mostrar: las preguntas y las posibles respuestas
 --> Se deben de contestar
-
 --> Cada pregunta correcta vale 100 puntos (Mostrar puntaje final)
 --> Botón de crear nueva trivia
-
 Cosas a tener en cuenta:Github pages 
 Extras: SCSS, BEM, Webpack */
+
+const triviaSection = document.querySelector(".trivia__creation");
+
+const triviaAPI = fetch(
+  "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
+);
+
+triviaAPI
+  .then((response) => response.json())
+  .then((data) => {
+    let resultados = data.results;
+    showData(resultados);
+  })
+  .catch((error) => console.log(error, error.message));
+
+const showData = (resultados) => {
+  console.log(resultados);
+  let body = "";
+  resultados.forEach((element, index) => {
+    body += `
+        <div class="tivia__container">
+          <p class="trivia__question">${index + 1} ${element.question}</p>
+          <ul class="trivia__list">
+            <li class="trivia__list--el">
+                <input type="radio" name="answer" class="trivia__answer" id="a">${
+                  element.incorrect_answers[0]
+                }</input>
+            </li>
+            <li class="trivia__list--el">
+                <input type="radio" name="answer" class="trivia__answer" id="b">${
+                  element.incorrect_answers[1]
+                }</input>
+            </li>
+            <li>
+                <input type="radio" name="answer" class="trivia__answer" id="c">${
+                  element.incorrect_answers[2]
+                }</input>
+            </li>
+            <li class="trivia__list--el">
+                <input type="radio" name="answer" class="trivia__answer" id="d">${
+                  element.correct_answer
+                }</input>
+            </li>
+          </ul>
+        </div>
+    `;
+  });
+  triviaSection.innerHTML = body;
+};
